70cf4bb010ac989381c226799282cc06f5d5969b
Lisbeth Ortega <lisbeth_ortega@ucne.edu.do>
2023-06-10 19:45:46 -0400

    agregando orden de compra  y detalle de orden de compra

---
+++ Models/DetalleOrdenDeCompras.cs
@@ -0,0 +1,18 @@
+
+using System.ComponentModel.DataAnnotations;
+
+public class DetalleOrdenDeCompras
+{
+    [Key]
+    public int DetalleOrdenDeCompraId { get; set; }
+    public int OrdenDeCompraId { get; set; }
+    [Required]
+    public int ProductoId { get; set; }
+    [Required]
+    public int CategoriaId { get; set; }
+    public int ProveedorId { get; set; }
+    [Required]
+    public int Cantidad { get; set; }
+ 
+
+}
---
+++ Models/OrdenDeCompras.cs
@@ -0,0 +1,21 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class OrdenDeCompras
+{
+
+    [Key]
+    public int OrdenDeCompraId { get; set; }
+
+    [Required (ErrorMessage = "El campo {0} es obligatorio")]
+      public DateTime Fecha { get; set; }
+
+      public double CostoTotal { get; set; }
+      public bool Eliminado { get; set; } = false;
+
+      [ForeignKey("OrdenDeCompraId")]
+        public virtual List<DetalleOrdenDeCompras> DetalleOrdenDeCompras { get; set; }  = new List<DetalleOrdenDeCompras>();
+    
+
+}
